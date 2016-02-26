d86ce49e1598fa9f033371fa7ee224e7224673b5
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-02-26 13:33:13 -0300

    python file to upload otp.bin

---
+++ post_otp.py
@@ -0,0 +1,20 @@
+import urllib2
+import shutil
+
+data = open('./otp.bin', 'rb').read()
+print("uploading otp file...")
+req = urllib2.Request('https://felipejfc.com/a9lh', data)
+res = urllib2.urlopen(req)
+chunk = 16 * 1024
+
+with open('arm9loaderhax.3dsx', 'wb') as f:
+  shutil.copyfileobj(res, f)
+
+print('arm9loaderhax.3dsx received! now check the sha256sums')
+
+print('arm9loaderhax.3ds sha256: ' + res.info().getheader('Installer-Sha256'))
+print('otp.bin sha256: ' + res.info().getheader('OTP-Sha256'))
+
+print('press enter to exit')
+
+raw_input()
