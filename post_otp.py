5359bdd8331d5f8af5b2879b8b60d11cf9560469
Mrrraou Mrrraou <totofvul@gmail.com>
2016-02-28 17:21:40 +0100

    Fixed a possible security issue. Made it more readable, and fixed some typos.

--- post_otp.py
+++ post_otp.py
@@ -2,18 +2,17 @@ import urllib2
 import shutil
 
 data = open('./otp.bin', 'rb').read()
-print("uploading otp file...")
+print("Uploading OTP file...")
 req = urllib2.Request('https://felipejfc.com/a9lh', data)
 res = urllib2.urlopen(req)
 
 with open('arm9loaderhax.3dsx', 'wb') as f:
-  shutil.copyfileobj(res, f)
+	shutil.copyfileobj(res, f)
 
 print('arm9loaderhax.3dsx received! now check the sha256sums')
 
-print('arm9loaderhax.3ds sha256: ' + res.info().getheader('Installer-Sha256'))
+print('arm9loaderhax.3dsx sha256: ' + res.info().getheader('Installer-Sha256'))
 print('otp.bin sha256: ' + res.info().getheader('OTP-Sha256'))
 
-print('press enter to exit')
-
+print('Press Enter to exit.')
 raw_input()
